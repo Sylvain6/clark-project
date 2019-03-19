f9eed504982ab531946d44522c1547b6e753937a
Sylvain Coutrot <s.coutrot.ext_azu@sodexo.com>
2019-03-19 11:26:51 +0100

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,11 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
+
+axios.get('https://api.ozae.com/gnw/articles?date=20190317__20190317&edition=fr-fr&key=11116dbf000000000000960d2228e999&hard_limit=50&topic=s')
+  .then(response => console.log(response));
 
 export default {
   name: 'home',
